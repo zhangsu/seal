5fca1587ab692e6ca7eeb378d3e0772572c0e8b8
Su Zhang <0x82cf@gmail.com>
2011-10-08 15:16:31 -0400

    Adjust Quoted Literal Style for Consistency

--- demo/playback.rb
+++ demo/playback.rb
@@ -3,7 +3,7 @@ require './seal'
 Audio.startup
 
 src = Audio::Source.new
-src.stream = Audio::Stream.open("pipa.ogg")
+src.stream = Audio::Stream.open('pipa.ogg')
 src.play
 while src.state == :playing
   src.update
