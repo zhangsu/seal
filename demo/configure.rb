8b3b6762f78d26355dfcd97014ab81ac1773e251
Su Zhang <0x82cf@gmail.com>
2011-10-17 00:56:47 -0400

    Implement Build Script for Linux

--- demo/configure.rb
+++ demo/configure.rb
@@ -6,21 +6,23 @@ include Config
 
 target_os = CONFIG['target_os']
 root_dir = '..'
-include_dir = "#{root_dir}/include"
 lib_dir = nil
 src_dir = "#{root_dir}/src"
 
 $defs << '-DNDEBUG'
 
 if target_os =~ /mswin|mingw/
+  include_dir = "#{root_dir}/include"
   lib_dir = "#{root_dir}/lib/win32"
   cp "#{lib_dir}/libmpg123.dll", '.'
   cp "#{lib_dir}/OpenAL32.dll", '.'
   cp "#{lib_dir}/wrap_oal.dll", '.'
   have_library('OpenAL32', 'alcOpenDevice')
+  have_library('libmpg123', 'mpg123_init')
 elsif target_os =~ /linux/
-  lib_dir = "#{root_dir}/lib/linux32"
-  # TODO
+  include_dir = "#{root_dir}"
+  have_library('openal', 'alcOpenDevice')
+  have_library('mpg123', 'mpg123_init')
 else
   puts 'This configuration does not support the current operating system.'
   exit
@@ -30,5 +32,4 @@ end
 $VPATH << src_dir << "#{src_dir}/seal"
 $VPATH << "#{src_dir}/libogg" << "#{src_dir}/libvorbis"
 dir_config('seal', include_dir, lib_dir)
-have_library('libmpg123', 'mpg123_init')
 create_makefile('seal', '../src/**')
