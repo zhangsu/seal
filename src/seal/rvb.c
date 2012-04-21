3e46a6808b0c9f39defb71df3ae504df89fc75b7
Su Zhang <0x82cf@gmail.com>
2012-04-21 15:19:39 -0400

    Add all reverb presets (C part)

--- src/seal/rvb.c
+++ src/seal/rvb.c
@@ -47,16 +47,155 @@ seal_destroy_rvb(seal_rvb_t* rvb)
     return _seal_destroy_obj(rvb, alDeleteEffects, alIsEffect);
 }
 
+/* A macro tied very closely to `seal_load_rvb_preset'. */
+#define CASE_LOAD(rvb, preset)                                              \
+    case SEAL_##preset##_REVERB:                                            \
+        LOAD_PROPERTIES(rvb, EFX_REVERB_PRESET_##preset);                   \
+        break;
+
 seal_err_t
 seal_load_rvb_preset(seal_rvb_t* rvb, seal_rvb_preset_t preset)
 {
     switch (preset) {
-    case SEAL_GENERIC_REVERB:
-        LOAD_PROPERTIES(rvb, EFX_REVERB_PRESET_GENERIC);
-        break;
-    case SEAL_MOUNTAINS_REVERB:
-        LOAD_PROPERTIES(rvb, EFX_REVERB_PRESET_MOUNTAINS);
-        break;
+        CASE_LOAD(rvb, GENERIC)
+        CASE_LOAD(rvb, PADDEDCELL)
+        CASE_LOAD(rvb, ROOM)
+        CASE_LOAD(rvb, BATHROOM)
+        CASE_LOAD(rvb, LIVINGROOM)
+        CASE_LOAD(rvb, STONEROOM)
+        CASE_LOAD(rvb, AUDITORIUM)
+        CASE_LOAD(rvb, CONCERTHALL)
+        CASE_LOAD(rvb, CAVE)
+        CASE_LOAD(rvb, ARENA)
+        CASE_LOAD(rvb, HANGAR)
+        CASE_LOAD(rvb, CARPETEDHALLWAY)
+        CASE_LOAD(rvb, HALLWAY)
+        CASE_LOAD(rvb, STONECORRIDOR)
+        CASE_LOAD(rvb, ALLEY)
+        CASE_LOAD(rvb, FOREST)
+        CASE_LOAD(rvb, CITY)
+        CASE_LOAD(rvb, MOUNTAINS)
+        CASE_LOAD(rvb, QUARRY)
+        CASE_LOAD(rvb, PLAIN)
+        CASE_LOAD(rvb, PARKINGLOT)
+        CASE_LOAD(rvb, SEWERPIPE)
+        CASE_LOAD(rvb, UNDERWATER)
+        CASE_LOAD(rvb, DRUGGED)
+        CASE_LOAD(rvb, DIZZY)
+        CASE_LOAD(rvb, PSYCHOTIC)
+
+        /* Castle Presets */
+        CASE_LOAD(rvb, CASTLE_SMALLROOM)
+        CASE_LOAD(rvb, CASTLE_SHORTPASSAGE)
+        CASE_LOAD(rvb, CASTLE_MEDIUMROOM)
+        CASE_LOAD(rvb, CASTLE_LARGEROOM)
+        CASE_LOAD(rvb, CASTLE_LONGPASSAGE)
+        CASE_LOAD(rvb, CASTLE_HALL)
+        CASE_LOAD(rvb, CASTLE_CUPBOARD)
+        CASE_LOAD(rvb, CASTLE_COURTYARD)
+        CASE_LOAD(rvb, CASTLE_ALCOVE)
+
+        /* Factory Presets */
+        CASE_LOAD(rvb, FACTORY_SMALLROOM)
+        CASE_LOAD(rvb, FACTORY_SHORTPASSAGE)
+        CASE_LOAD(rvb, FACTORY_MEDIUMROOM)
+        CASE_LOAD(rvb, FACTORY_LARGEROOM)
+        CASE_LOAD(rvb, FACTORY_LONGPASSAGE)
+        CASE_LOAD(rvb, FACTORY_HALL)
+        CASE_LOAD(rvb, FACTORY_CUPBOARD)
+        CASE_LOAD(rvb, FACTORY_COURTYARD)
+        CASE_LOAD(rvb, FACTORY_ALCOVE)
+
+        /* Ice Palace Presets */
+        CASE_LOAD(rvb, ICEPALACE_SMALLROOM)
+        CASE_LOAD(rvb, ICEPALACE_SHORTPASSAGE)
+        CASE_LOAD(rvb, ICEPALACE_MEDIUMROOM)
+        CASE_LOAD(rvb, ICEPALACE_LARGEROOM)
+        CASE_LOAD(rvb, ICEPALACE_LONGPASSAGE)
+        CASE_LOAD(rvb, ICEPALACE_HALL)
+        CASE_LOAD(rvb, ICEPALACE_CUPBOARD)
+        CASE_LOAD(rvb, ICEPALACE_COURTYARD)
+        CASE_LOAD(rvb, ICEPALACE_ALCOVE)
+
+        /* Space Station Presets */
+        CASE_LOAD(rvb, SPACESTATION_SMALLROOM)
+        CASE_LOAD(rvb, SPACESTATION_SHORTPASSAGE)
+        CASE_LOAD(rvb, SPACESTATION_MEDIUMROOM)
+        CASE_LOAD(rvb, SPACESTATION_LARGEROOM)
+        CASE_LOAD(rvb, SPACESTATION_LONGPASSAGE)
+        CASE_LOAD(rvb, SPACESTATION_HALL)
+        CASE_LOAD(rvb, SPACESTATION_CUPBOARD)
+        CASE_LOAD(rvb, SPACESTATION_ALCOVE)
+
+        /* Wooden Galleon Presets */
+        CASE_LOAD(rvb, WOODEN_SMALLROOM)
+        CASE_LOAD(rvb, WOODEN_SHORTPASSAGE)
+        CASE_LOAD(rvb, WOODEN_MEDIUMROOM)
+        CASE_LOAD(rvb, WOODEN_LARGEROOM)
+        CASE_LOAD(rvb, WOODEN_LONGPASSAGE)
+        CASE_LOAD(rvb, WOODEN_HALL)
+        CASE_LOAD(rvb, WOODEN_CUPBOARD)
+        CASE_LOAD(rvb, WOODEN_COURTYARD)
+        CASE_LOAD(rvb, WOODEN_ALCOVE)
+
+        /* Sports Presets */
+        CASE_LOAD(rvb, SPORT_EMPTYSTADIUM)
+        CASE_LOAD(rvb, SPORT_SQUASHCOURT)
+        CASE_LOAD(rvb, SPORT_SMALLSWIMMINGPOOL)
+        CASE_LOAD(rvb, SPORT_LARGESWIMMINGPOOL)
+        CASE_LOAD(rvb, SPORT_GYMNASIUM)
+        CASE_LOAD(rvb, SPORT_FULLSTADIUM)
+        CASE_LOAD(rvb, SPORT_STADIUMTANNOY)
+
+        /* Prefab Presets */
+        CASE_LOAD(rvb, PREFAB_WORKSHOP)
+        CASE_LOAD(rvb, PREFAB_SCHOOLROOM)
+        CASE_LOAD(rvb, PREFAB_PRACTISEROOM)
+        CASE_LOAD(rvb, PREFAB_OUTHOUSE)
+        CASE_LOAD(rvb, PREFAB_CARAVAN)
+
+        /* Dome and Pipe Presets */
+        CASE_LOAD(rvb, DOME_TOMB)
+        CASE_LOAD(rvb, PIPE_SMALL)
+        CASE_LOAD(rvb, DOME_SAINTPAULS)
+        CASE_LOAD(rvb, PIPE_LONGTHIN)
+        CASE_LOAD(rvb, PIPE_LARGE)
+        CASE_LOAD(rvb, PIPE_RESONANT)
+
+        /* Outdoors Presets */
+        CASE_LOAD(rvb, OUTDOORS_BACKYARD)
+        CASE_LOAD(rvb, OUTDOORS_ROLLINGPLAINS)
+        CASE_LOAD(rvb, OUTDOORS_DEEPCANYON)
+        CASE_LOAD(rvb, OUTDOORS_CREEK)
+        CASE_LOAD(rvb, OUTDOORS_VALLEY)
+
+        /* Mood Presets */
+        CASE_LOAD(rvb, MOOD_HEAVEN)
+        CASE_LOAD(rvb, MOOD_HELL)
+        CASE_LOAD(rvb, MOOD_MEMORY)
+
+        /* Driving Presets */
+        CASE_LOAD(rvb, DRIVING_COMMENTATOR)
+        CASE_LOAD(rvb, DRIVING_PITGARAGE)
+        CASE_LOAD(rvb, DRIVING_INCAR_RACER)
+        CASE_LOAD(rvb, DRIVING_INCAR_SPORTS)
+        CASE_LOAD(rvb, DRIVING_INCAR_LUXURY)
+        CASE_LOAD(rvb, DRIVING_FULLGRANDSTAND)
+        CASE_LOAD(rvb, DRIVING_EMPTYGRANDSTAND)
+        CASE_LOAD(rvb, DRIVING_TUNNEL)
+
+        /* City Presets */
+        CASE_LOAD(rvb, CITY_STREETS)
+        CASE_LOAD(rvb, CITY_SUBWAY)
+        CASE_LOAD(rvb, CITY_MUSEUM)
+        CASE_LOAD(rvb, CITY_LIBRARY)
+        CASE_LOAD(rvb, CITY_UNDERPASS)
+        CASE_LOAD(rvb, CITY_ABANDONED)
+
+        /* Misc. Presets */
+        CASE_LOAD(rvb, DUSTYROOM)
+        CASE_LOAD(rvb, CHAPEL)
+        CASE_LOAD(rvb, SMALLWATERROOM)
     }
     return SEAL_OK;
 }
