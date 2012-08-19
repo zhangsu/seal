668551f181871cc4fd7af9bf5c60af7975511374
Su Zhang <me@zhang.su>
2012-08-19 01:00:05 -0400

    Complete tests for core module

--- spec/seal/core_spec.rb
+++ spec/seal/core_spec.rb
@@ -9,4 +9,19 @@ describe Seal do
   it 'should fail when trying to initialze non-existing device' do
     expect { Seal.startup 'foo42' }.to raise_error(SealError)
   end
+
+  it 'has only one instance of Listener' do
+    Seal.listener.should == Seal.listener
+    Seal.listener.is_a?(Listener).should be_true
+    expect { Seal::Listener.new }.to raise_error(NoMethodError)
+    expect { Seal::Listener.allocate }.to raise_error(TypeError)
+  end
+
+  it 'defines a limit on the number of effect slots per source' do
+    Seal.per_source_effect_limit.is_a?(Integer).should be_true
+  end
+
+  it 'defines a version string' do
+    Seal::VERSION.is_a?(String).should be_true
+  end
 end
