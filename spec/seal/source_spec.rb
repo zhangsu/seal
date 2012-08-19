2383ddd99e3a40381cc51a08a78a9132a42e635e
Su Zhang <me@zhang.su>
2012-08-19 02:05:53 -0400

    Add default attribute value test for sources

---
+++ spec/seal/source_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe Source do
+  around :each do |example|
+    Seal.startup
+    example.run
+    Seal.cleanup
+  end
+
+  describe 'default attributes' do
+    let(:source) { Source.new }
+
+    it 'is automatic' do
+      source.auto?.should be_true
+    end
+
+    it 'has no buffer' do
+      source.buffer.should be_nil
+    end
+
+    it 'has a chunk size of 36864' do
+      source.chunk_size.should == 36864
+    end
+
+    it 'has a gain of 1.0' do
+      source.gain.should be_within(0.01).of(1.0)
+    end
+
+    it 'is not looping' do
+      source.looping.should be_false
+    end
+
+    it 'has a pitch of 1.0' do
+      source.pitch.should be_within(0.01).of(1.0)
+    end
+
+    it 'has a position of (0,0,0)' do
+      source.position.should == [0, 0, 0]
+    end
+
+    it 'has a queue size of 3' do
+      source.queue_size.should == 3
+    end
+
+    it 'is not relative' do
+      source.relative.should be_false
+    end
+
+    it 'is in the initial state' do
+      source.state.should == Source::State::INITIAL
+    end
+
+    it 'has no stream' do
+      source.stream.should be_nil
+    end
+
+    it 'has un undertermined type' do
+      source.type.should == Source::Type::UNDETERMINED
+    end
+
+    it 'has a velocity of (0,0,0)' do
+      source.velocity.should == [0, 0, 0]
+    end
+  end
+end
