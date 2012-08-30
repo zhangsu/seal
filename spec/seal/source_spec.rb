fac9013201ccbe9e2274e22c40b07bc1bdce8c68
Su Zhang <me@zhang.su>
2012-08-29 20:58:03 -0400

    Reduce the number of times sources are generated
    No need to allocate a new source for each examples within a group.

--- spec/seal/source_spec.rb
+++ spec/seal/source_spec.rb
@@ -2,54 +2,56 @@ require 'spec_helper'
 
 describe Source do
   describe 'default attributes' do
-    let(:source) { Source.new }
+    before :all do
+      @source = Source.new
+    end
 
     it 'is automatic' do
-      source.auto?.should be_true
+      @source.auto?.should be_true
     end
 
     it 'has no buffer' do
-      source.buffer.should be_nil
+      @source.buffer.should be_nil
     end
 
     it 'has a chunk size of 36864' do
-      source.chunk_size.should == 36864
+      @source.chunk_size.should == 36864
     end
 
     it 'has a gain of 1.0' do
-      source.gain.should be_within(TOLERANCE).of(1.0)
+      @source.gain.should be_within(TOLERANCE).of(1.0)
     end
 
     it 'is not looping' do
-      source.looping.should be_false
+      @source.looping.should be_false
     end
 
     it 'has a pitch of 1.0' do
-      source.pitch.should be_within(TOLERANCE).of(1.0)
+      @source.pitch.should be_within(TOLERANCE).of(1.0)
     end
 
     it 'has a position of (0,0,0)' do
-      source.position.should == [0, 0, 0]
+      @source.position.should == [0, 0, 0]
     end
 
     it 'has a queue size of 3' do
-      source.queue_size.should == 3
+      @source.queue_size.should == 3
     end
 
     it 'is not relative' do
-      source.relative.should be_false
+      @source.relative.should be_false
     end
 
     it 'is in the initial state' do
-      source.state.should == Source::State::INITIAL
+      @source.state.should == Source::State::INITIAL
     end
 
     it 'has no stream' do
-      source.stream.should be_nil
+      @source.stream.should be_nil
     end
 
     it 'has un undertermined type' do
-      source.type.should == Source::Type::UNDETERMINED
+      @source.type.should == Source::Type::UNDETERMINED
     end
 
     it 'has a velocity of (0,0,0)' do
