df4a324463a3ba808512834387a9d53ff67896e8
Su Zhang <me@zhang.su>
2012-09-28 01:07:10 -0400

    Add buffer size test

--- spec/seal/buffer_spec.rb
+++ spec/seal/buffer_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 describe Buffer do
+  it_behaves_like 'an audio object with format'
+
+  describe 'size after reading' do
+    context 'from the WAVE file' do
+      subject { Buffer.new(WAV_PATH) }
+      its(:size) { should eq 2250 }
+    end
+
+    context 'from the Ogg Vorbise file' do
+      subject { Buffer.new(OV_PATH) }
+      its(:size) { should eq 85342 }
+    end
+  end
+
   it 'cannot be changed if it is being used by a source' do
     error_pattern = /Invalid operation/
     source = Source.new
@@ -20,6 +34,4 @@ describe Buffer do
     # Not being used by any source.
     expect { buffer.load(WAV_PATH) }.to_not raise_error
   end
-
-  it_behaves_like 'an audio object with format'
 end
