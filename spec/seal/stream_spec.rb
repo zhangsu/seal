d536c3c3a1898c2b35f37e99529338b883efa7b4
Su Zhang <me@zhang.su>
2012-09-28 23:35:33 -0400

    Finish the rest of stream tests

--- spec/seal/stream_spec.rb
+++ spec/seal/stream_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
+include Source::State
+
 describe Stream do
+  let(:source) do
+    Source.new.tap { |source| source.stream = Stream.new(WAV_PATH) }
+  end
+
   it_behaves_like 'an audio object with format'
 
   context 'that are closed' do
@@ -11,5 +17,22 @@ describe Stream do
     its(:bit_depth) { should eq 16 }
     its(:channel_count) { should eq 1 }
     its(:frequency) { should eq 0 }
+  end
+
+  example 'rewinding prevents source from stopping' do
+    source.play
+    3.times do
+      sleep(0.2)
+      source.stream.rewind
+    end
+    source.state.should be PLAYING
+  end
+
+  it 'can be closed even when used by sources' do
+    expect do
+      source.play
+      source.stream.close
+      sleep 0.5
+    end.to_not raise_error
   end
 end
