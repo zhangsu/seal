b13351838eb1a2b292265b3ef2a46433b636fa1e
Su Zhang <me@zhang.su>
2012-12-02 23:33:30 -0800

    Add win32api binding for listener gain

--- win32api/listener.rb
+++ win32api/listener.rb
@@ -2,12 +2,25 @@ require './core'
 
 module Seal
   class Listener
+    SET_GAIN = SealAPI.new('set_listener_gain', 'i', 'i')
+    GET_GAIN = SealAPI.new('get_listener_gain', 'p', 'i')
     SET_POSITION = SealAPI.new('set_listener_pos', 'iii', 'i')
     GET_POSITION = SealAPI.new('get_listener_pos', 'ppp', 'i')
 
-    def position=(pos)
-      CHECK_ERROR[SET_POSITION[*pos.pack('f*').unpack('i*')]]
-      pos
+    def gain=(gain)
+      CHECK_ERROR[SET_GAIN[[gain].pack('f').unpack('i')[0]]]
+      gain
+    end
+
+    def gain
+      gain_buffer = '    '
+      CHECK_ERROR[GET_GAIN[gain_buffer]]
+      return gain_buffer.unpack('f')[0]
+    end
+
+    def position=(position)
+      CHECK_ERROR[SET_POSITION[*position.pack('f*').unpack('i*')]]
+      position
     end
 
     def position
