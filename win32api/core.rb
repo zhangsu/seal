a57851ba52513dd5f566566899ca8cee5ce409f5
Su Zhang <me@zhang.su>
2012-12-02 22:47:51 -0800

    Abstract common stuff into core.rb
    And make seal.rb a frontend.

---
+++ win32api/core.rb
@@ -0,0 +1,33 @@
+require './sealapi'
+
+module Seal
+  VERSION = SealAPI.new('get_version', 'v', 'p')[]
+
+  get_err_msg = SealAPI.new('get_err_msg', 'i', 'p')
+  CHECK_ERROR = lambda do |error_code|
+    raise SealError, get_err_msg[error_code] if error_code != 0
+    nil
+  end
+
+  class << self
+    STARTUP = SealAPI.new('startup', 'p', 'i')
+    CLEANUP = SealAPI.new('cleanup', 'v', 'v')
+    GET_PER_SRC_EFFECT_LIMIT =
+      SealAPI.new('get_per_src_effect_limit', 'v', 'i')
+
+    def startup(device = nil)
+      CHECK_ERROR[STARTUP[device]]
+    end
+
+    def cleanup
+      CLEANUP[]
+    end
+
+    def per_source_effect_limit
+      GET_PER_SRC_EFFECT_LIMIT[]
+    end
+  end
+
+  class SealError < Exception
+  end
+end
--- win32api/seal.rb
+++ win32api/seal.rb
@@ -2,40 +2,6 @@
 #
 # This is a fallback solution just in case Ruby API and DL are unavailable.
 # Performance-wise, Win32API < DL < Ruby API.
-# This is a fallback solution just in case Ruby API and DL are unavailable.
-# Performance-wise, Win32API < DL < Ruby API.
 
-require_relative 'sealapi'
-
-module Seal
-  VERSION = SealAPI.new('get_version', 'v', 'p')[]
-
-  class << self
-    GET_ERR_MSG = SealAPI.new('get_err_msg', 'i', 'p')
-    STARTUP = SealAPI.new('startup', 'p', 'i')
-    CLEANUP = SealAPI.new('cleanup', 'v', 'v')
-    GET_PER_SRC_EFFECT_LIMIT = SealAPI.new('get_per_src_effect_limit',
-                                           'v', 'i')
-
-    def startup(device = nil)
-      _check_error(STARTUP[device])
-    end
-
-    def cleanup
-      CLEANUP[]
-    end
-
-    def per_source_effect_limit
-      GET_PER_SRC_EFFECT_LIMIT[]
-    end
-
-    def _check_error(error_code)
-      raise SealError, GET_ERR_MSG.call(error_code) if error_code != 0
-      nil
-    end
-  end
-
-  class SealError < Exception
-  end
-
-end
+require './core'
+require './listener'
