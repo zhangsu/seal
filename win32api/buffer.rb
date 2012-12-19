3d83ae52618f80e526c8fcced4d903b32de00ff7
Su Zhang <me@zhang.su>
2012-12-19 00:37:21 -0800

    Add Win32API binding for buffers

--- win32api/buffer.rb
+++ win32api/buffer.rb
@@ -1,4 +1,40 @@
+require File.join(File.dirname(__FILE__), 'core')
+
 module Seal
   class Buffer
+    INIT_BUF = SealAPI.new('init_buf', 'p', 'i')
+    LOAD2BUF = SealAPI.new('load2buf', 'ppi', 'i')
+    GET_BUF_SIZE = SealAPI.new('get_buf_size', 'pp', 'i')
+    GET_BUF_FREQ = SealAPI.new('get_buf_freq', 'pp', 'i')
+    GET_BUF_BPS = SealAPI.new('get_buf_bps', 'pp', 'i')
+    GET_BUF_NCHANNELS = SealAPI.new('get_buf_nchannels', 'pp', 'i')
+
+    def initialize(filename, format = Format::UNKNOWN)
+      @buffer = '    '
+      CHECK_ERROR[INIT_BUF[@buffer]]
+      INPUT_AUDIO[@buffer, LOAD2BUF, filename, format]
+      self
+    end
+
+    def load(filename, format = Format::UNKNOWN)
+      INPUT_AUDIO[@buffer, LOAD2BUF, filename, format]
+      self
+    end
+
+    def size
+      GET_OBJ_INT[@buffer, GET_BUF_SIZE]
+    end
+
+    def frequency
+      GET_OBJ_INT[@buffer, GET_BUF_FREQ]
+    end
+
+    def bit_depth
+      GET_OBJ_INT[@buffer, GET_BUF_BPS]
+    end
+
+    def channel_count
+      GET_OBJ_INT[@buffer, GET_BUF_NCHANNELS]
+    end
   end
 end
--- win32api/core.rb
+++ win32api/core.rb
@@ -1,13 +1,29 @@
 require File.join(File.dirname(__FILE__), 'sealapi')
 
 module Seal
-  VERSION = SealAPI.new('get_version', 'v', 'p')[]
-
+  # Define helpers used by Win32API binding code.
   get_err_msg = SealAPI.new('get_err_msg', 'i', 'p')
   CHECK_ERROR = lambda do |error_code|
     raise SealError, get_err_msg[error_code] if error_code != 0
     nil
   end
+
+  INPUT_AUDIO = lambda do |media, input, filename, format|
+    CHECK_ERROR[input[media, filename, format]]
+  end
+
+  get_obj_attr = lambda do |obj, get, type|
+    buffer = '    '
+    CHECK_ERROR[get[obj, buffer]]
+    buffer.unpack(type)[0]
+  end
+
+  GET_OBJ_INT = lambda do |obj, get|
+    get_obj_attr[obj, get, 'i']
+  end
+
+
+  VERSION = SealAPI.new('get_version', 'v', 'p')[]
 
   class << self
     STARTUP = SealAPI.new('startup', 'p', 'i')
@@ -29,9 +45,10 @@ module Seal
   end
 
   module Format
-    WAV = :wav
-    OV = :ov
-    MPG = :mpg
+    UNKNOWN = 0
+    WAV = 1
+    OV = 2
+    MPG = 3
   end
 
   class SealError < Exception
