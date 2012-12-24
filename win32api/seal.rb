8994e5d60827f699a9e52e35e5ceb3aef98f7a07
Su Zhang <me@zhang.su>
2012-12-23 23:15:41 -0800

    Refactor to be less repetitive

--- win32api/seal.rb
+++ win32api/seal.rb
@@ -4,10 +4,12 @@
 # Performance-wise, Win32API < DL < Ruby API.
 
 current_dir = File.dirname(__FILE__)
-require File.join(current_dir, 'core')
-require File.join(current_dir, 'listener')
-require File.join(current_dir, 'buffer')
-require File.join(current_dir, 'effect_slot')
-require File.join(current_dir, 'reverb')
-require File.join(current_dir, 'source')
-require File.join(current_dir, 'stream')
+[
+  'core',
+  'listener',
+  'buffer',
+  'effect_slot',
+  'reverb',
+  'source',
+  'stream'
+].each { |mod| require File.join(current_dir, mod) }
