75b5b293af30fbc6f7149b3848e4e2897114a36b
Su Zhang <me@zhang.su>
2012-12-26 14:53:09 -0800

    Define SealAPI under the Seal namespace

--- win32api/seal_api.rb
+++ win32api/seal_api.rb
@@ -1,26 +1,26 @@
 require 'win32api' unless defined? Win32API
 
-LIB_DIR = '.' unless defined? LIB_DIR
+module Seal
+  class SealAPI < Win32API
+    STRCPY_S = Win32API.new('msvcrt', 'strcpy_s', 'pll', 'i')
 
-class SealAPI < Win32API
-  STRCPY_S = Win32API.new('msvcrt', 'strcpy_s', 'pll', 'i')
-
-  def initialize(func, arg_types, return_type = 'i', *args)
-    @return_string = return_type == 'p'
-    library = File.join(LIB_DIR, 'seal')
-    super(library, "seal_#{func}", arg_types, return_type, *args)
-  end
-
-  def [](*args)
-    result = call(*args)
-    if @return_string and result.is_a? Integer
-      # String pointer is returned to Ruby as an integer even though we
-      # specified 'p' as the return value - possibly a bug in Ruby 1.9.3's
-      # Win32API implementation. Work around it.
-      message_buffer = ' ' * 128
-      STRCPY_S.call(message_buffer, message_buffer.size, result)
-      return message_buffer.strip
+    def initialize(func, arg_types, return_type = 'i', *args)
+      @return_string = return_type == 'p'
+      library = File.join(defined?(LIB_DIR) ? LIB_DIR : '.', 'seal')
+      super(library, "seal_#{func}", arg_types, return_type, *args)
     end
-    result
-  end unless method_defined? :[]
+
+    def [](*args)
+      result = call(*args)
+      if @return_string and result.is_a? Integer
+        # String pointer is returned to Ruby as an integer even though we
+        # specified 'p' as the return value - possibly a bug in Ruby 1.9.3's
+        # Win32API implementation. Work around it.
+        message_buffer = ' ' * 128
+        STRCPY_S.call(message_buffer, message_buffer.size, result)
+        return message_buffer.strip
+      end
+      result
+    end unless method_defined? :[]
+  end
 end
