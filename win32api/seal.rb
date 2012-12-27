9a2c039720a62db3094a9e89c135d8899484f6cb
Su Zhang <me@zhang.su>
2012-12-26 21:12:03 -0800

    Simplify token collection expression

--- win32api/seal.rb
+++ win32api/seal.rb
@@ -4,12 +4,6 @@
 # Performance-wise, Win32API < DL < Ruby API.
 
 current_dir = File.dirname(__FILE__)
-[
-  'core',
-  'listener',
-  'buffer',
-  'effect_slot',
-  'reverb',
-  'source',
-  'stream'
-].each { |mod| require File.join(current_dir, mod) }
+%w[core listener buffer effect_slot reverb source stream].each do |mod|
+  require File.join(current_dir, mod)
+end
