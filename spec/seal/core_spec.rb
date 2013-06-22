4e06e6ec92ad440f1bafef8245bc7d7046dab0f5
Su Zhang <suzhang@google.com>
2013-06-21 22:55:37 -0700

    Fix Startup test error on some devices
    Calling startup twice in a row causes failure for some devices. See
    added comments above the added before filter for details.

--- spec/seal/core_spec.rb
+++ spec/seal/core_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
 describe Seal do
-  it 'should initialize and finalize correctly on default device' do
-    expect do
-      Seal.startup
-      Seal.cleanup
-    end.to_not raise_error
-  end
+  context 'initially' do
+    # Examples in this group needs to call Seal.startup themselves instead of
+    # relying on it being implicitly called by the global before hook.
+    # Therefore, Seal must be finalized in order to avoid double startup, which
+    # may fail for some devices due to opening an already opened, busy device.
+    before { Seal.cleanup }
+    # Since there is a global after hook for Seal.cleanup, we insert this after
+    # hook before the global one so the global one will then cleans up
+    # properly.
+    prepend_after { Seal.startup }
 
-  it 'should fail when trying to initialize non-existing device' do
-    expect { Seal.startup 'foo42' }.to raise_error SealError
+    it 'starts up and cleans up properly on default device' do
+      expect do
+        Seal.startup
+        Seal.cleanup
+      end.to_not raise_error
+    end
+
+    it 'fails when trying to start up on non-existing device' do
+      expect { Seal.startup 'foo42' }.to raise_error SealError
+    end
   end
 
   it 'has only one instance of Listener' do
