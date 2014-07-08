d864aa6584b904329c2de0b83a58c6d376c87e72
Su Zhang <me@zhang.su>
2014-07-07 23:59:14 -0400

    Use expect syntax in boolean_reader_aliases

--- spec/support/boolean_reader_aliases.rb
+++ spec/support/boolean_reader_aliases.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 shared_examples 'boolean reader aliases' do |method_names|
   method_names.each do |name|
     specify "#{name}? is an alias of #{name}" do
-      subject.method(name).should eq subject.method("#{name}?")
+      expect(subject.method(name)).to eq subject.method("#{name}?")
     end
   end
 end
