6a970d6568a4679a8f3d0bfb95490399e1c19fac
Su Zhang <me@zhang.su>
2014-07-12 01:02:17 -0400

    Use expect syntax in movable_object

--- spec/support/movable_object.rb
+++ spec/support/movable_object.rb
@@ -2,22 +2,24 @@ require 'spec_helper'
 
 shared_examples 'a movable object' do
   describe 'by default' do
-    its(:position) { should eq [0, 0, 0] }
-    its(:velocity) { should eq [0, 0, 0] }
+    its(:position) { is_expected.to eq [0, 0, 0] }
+    its(:velocity) { is_expected.to eq [0, 0, 0] }
   end
 
   context 'with motion', :include_helpers do
     it 'changes its position' do
       with_motion(subject) do
         subject.position = 3.2, 3.1, -0.3
-        subject.position.should be_each_within(TOLERANCE).of [3.2, 3.1, -0.3]
+        expect(subject.position).to be_each_within(TOLERANCE)
+          .of [3.2, 3.1, -0.3]
       end
     end
 
     it 'changes its velocity' do
       with_motion(subject) do
         subject.velocity = 1.3, -3, 0.566
-        subject.velocity.should be_each_within(TOLERANCE).of [1.3, -3, 0.566]
+        expect(subject.velocity).to be_each_within(TOLERANCE)
+          .of [1.3, -3, 0.566]
       end
     end
 
@@ -28,7 +30,7 @@ shared_examples 'a movable object' do
           position = subject.position
           new_position = position.zip(subject.velocity).map { |x, y| x + y }
           subject.move
-          subject.position.should be_each_within(TOLERANCE).of new_position
+          expect(subject.position).to be_each_within(TOLERANCE).of new_position
         end
       end
     end
